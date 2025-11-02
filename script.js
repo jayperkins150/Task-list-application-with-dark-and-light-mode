21d5332231238608dfb3f72a36a61e20de6d4912
jayperkins150 <jayperkins150@yahoo.co.uk>
2025-11-02 10:02:30 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// Create array to store tasks
+let tasks = [];
+
+// Function to add new tasks
+function addTask() {
+    const taskInput = document.getElementById("taskInput").value;
+    if (taskInput) {
+        tasks.push(taskInput); // Add new task to the tasks array
+        document.getElementById("taskInput").value = ""; // Delete text in the task input field
+        displayTasks(); // Display the updated tasks list
+    } else {
+        alert("Please enter a task"); // Alert if a task has not been entered into the task input field
+    }
+}
+
+// Function to display tasks
+function displayTasks() {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = ""; // Clear current tasks list
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li"); // Create new list item for a task
+        li.textContent = task; // Add the text input to the task item
+        const removeButton = document.createElement("button"); // Create remove item button
+        removeButton.textContent = "Remove item"; // Set text content for remove item button
+        removeButton.onclick = () => removeTask(index); // Set the onClick event to remove a task
+        li.appendChild(removeButton); // Add the remove button to the list item
+        taskList.appendChild(li); // Add the list item to the task list
+    });
+}
+
+// Function to remove tasks
+function removeTask(index) {
+    tasks.splice(index, 1); // Remove task from the tasks list
+    displayTasks(); // Display the updated tasks list
+}
+
+
+
+// Dark and light mode toggle
+
+
 //Select the dark mode button
 const toggleButton = document.getElementById("toggleButton");
 
@@ -21,39 +62,3 @@ function toggleMode() {
 
 //Add event listener to the toggle button
 toggleButton.addEventListener("click", toggleMode);
-
-
-//Array to store tasks
-let tasks = [];
-
-//Function to add new tasks
-function addTask() {
-    const taskInput = document.getElementById("taskInput").value;
-    if (taskInput) {
-        tasks.push(taskInput); //Add new task to the tasks array
-        document.getElementById("taskInput").value = ""; //Delete text in the task input field
-        displayTasks(); //Display the updated tasks list
-    } else {
-        alert("Please enter a task."); //Alert if the input field is empty
-    }
-}
-
-//Function to display tasks
-function displayTasks() {
-    const taskList = document.getElementById("taskList");
-    taskList.innerHTML = ""; //Clear the current tasks list
-    tasks.forEach((tasks, index) => {
-        const li = document.createElement("li"); //Create a list item for each task
-        li.textContent = tasks; //Set the text of the list item to the task
-        const removeButton = document.createElement("button"); //Create remove item button
-        removeButton.textContent = "Remove"; //Set the text of the remove item button
-        removeButton.onClick = () => removeTask(index); //Set the onClick event to remove a task
-        li.appendChild(removeButton); //Add the remove button to the list item
-        taskList.appendChild(li); //Add the list item to the task list
-    });
-}
-
-function removeTask(index) {
-    tasks.splice(index, 1); //Remove task from the tasks list
-    displayTasks(); //Display the updated tasks list
-}
